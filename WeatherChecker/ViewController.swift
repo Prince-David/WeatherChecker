0cc4c1de0fc6bc28f4999da2bb63cc7b8e7577a3
David Maiman <david.maiman@gmail.com>
2023-09-13 04:03:49 -0400

    Initial Commit

---
+++ WeatherChecker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherChecker
+//
+//  Created by DAVID MAIMAN on 9/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
