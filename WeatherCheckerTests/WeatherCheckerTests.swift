c9ffccb073071dc46be8cc403bd3d8a673a9d7f8
David Maiman <david.maiman@gmail.com>
2023-09-16 11:30:34 -0600

    Added some unit tests.

--- WeatherCheckerTests/WeatherCheckerTests.swift
+++ WeatherCheckerTests/WeatherCheckerTests.swift
@@ -6,30 +6,60 @@
 //
 
 import XCTest
+@testable import WeatherChecker
 
 final class WeatherCheckerTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    func testWeatherDecoding() {
+        let jsonData = """
+           {
+               "icon": "123",
+               "description": "light rain"
+           }
+           """.data(using: .utf8)!
+        
+        let weather = try? JSONDecoder().decode(Weather.self, from: jsonData)
+        
+        XCTAssertNotNil(weather)
+        XCTAssertEqual(weather?.icon, "123")
+        XCTAssertEqual(weather?.description, "light rain")
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testMainDecoding() {
+        let jsonData = """
+           {
+               "temp": 33.15
+           }
+           """.data(using: .utf8)!
+        
+        let main = try? JSONDecoder().decode(Main.self, from: jsonData)
+        
+        XCTAssertNotNil(main)
+        XCTAssertEqual(main?.temp, 33.15)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testWeatherResponseDecoding() {
+        let jsonData = """
+           {
+               "name": "New York",
+               "weather": [{
+                   "icon": "123",
+                   "description": "light rain"
+               }],
+               "main": {
+                   "temp": 33.15
+               }
+           }
+           """.data(using: .utf8)!
+        
+        let response = try? JSONDecoder().decode(WeatherResponse.self, from: jsonData)
+        
+        XCTAssertNotNil(response)
+        XCTAssertEqual(response?.main.temp, 33.15)
+        XCTAssertEqual(response?.name, "New York")
+        XCTAssertEqual(response?.weather.count, 1)
+        XCTAssertEqual(response?.weather[0].icon, "123")
+        XCTAssertEqual(response?.weather[0].description, "light rain")
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+    
 }
